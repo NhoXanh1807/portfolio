eeaa7d6b1ebfbbd421b223bb0623203863d20d0a
NhoXanh1807 <nguyenbanhatquang1807@gmail.com>
2025-11-11 00:54:46 +0700

    update experience

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "WangTe0f";
-export const SITE_DESCRIPTION = "Security Engineer WangTe0f sharing offensive research, detailed writeups, and professional experience.";
+export const SITE_DESCRIPTION = "Security Newbie WangTe0f sharing his knowledge ";
